44bfb50e11148aec16c06946dba8c787dad738e2
hayoung <kaia@TABLET-KTVIIBE9.>
2025-07-24 18:56:26 +0900

    home

--- src/components/common/BottomNav.js
+++ src/components/common/BottomNav.js
@@ -2,10 +2,12 @@
 import '../../styles/common/bottomnav.css';
 import { ReactComponent as FestivalIcon } from '../../assets/nav/festival.svg';
 import { ReactComponent as YoutubeIcon } from '../../assets/nav/youtube.svg';
+import { ReactComponent as TripIcon } from '../../assets/nav/trip.svg';
 
 const menus = [
-  { icon: <FestivalIcon />, label: '축제·행사' },
-  { icon: <YoutubeIcon />, label: '유튜브' }
+  { icon: <FestivalIcon />, label: '축제 정보' },
+  { icon: <YoutubeIcon />, label: '세종시 공식 유튜브' },
+  { icon: <TripIcon />, label: '추천 코스' }
 ];
 
 export default function BottomNav({ activeIdx = 0, onTabChange }) {
--- src/pages/MockHome.js
+++ src/pages/MockHome.js
@@ -1,24 +1,53 @@
+//scr/pages/MockHome.js
 import Background from '../components/common/Background';
 import SearchBar from '../components/common/SearchBar';
 import BottomNav from '../components/common/BottomNav';
-
+import logo from '../assets/nav/logo.svg';
 import mapImg from '../assets/nav/map.svg';
 
 export default function MockHome() {
   return (
     <Background>
       <div style={{ paddingTop: 32, paddingBottom: 84, minHeight: '100vh' }}>
-        {/* 서치바 */}
-        <SearchBar />
-
-        {/* 가운데 임시 텍스트 */}
-        <div style={{ margin: "60px 0 0 0", textAlign: "center", color: "#222", fontWeight: 600 }}>
-          App name? Hmm..
+        {/* 상단 로고 */}
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: 24 // 검색창과 간격 확보
+        }}>
+          <img
+            src={logo}
+            alt="로고"
+            style={{
+              width: "280px",     // 검색창 넓이
+              maxWidth: "96%",    // 화면 반응형 대응
+              height: "auto"
+            }}
+          />
         </div>
 
-        {/* 지도 이미지 삽입 */}
-        <div style={{ margin: "40px 0", display: "flex", justifyContent: "center" }}>
-          <img src={mapImg} alt="세종 지도" style={{ width: "80%", maxWidth: 340, height: "auto" }} />
+        {/* 서치바 */}
+        <div style={{ marginTop: 32 }}>
+          <SearchBar />
+        </div>
+
+        {/* 지도 이미지 */}
+        <div style={{
+          margin: "60px 0 0 0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          <img
+            src={mapImg}
+            alt="세종 지도"
+            style={{
+              width: "92vw",             // 필요시 px로 조절
+              maxWidth: 480,             // 최대 크기 제한
+              height: "auto",
+            }}
+          />
         </div>
       </div>
 
